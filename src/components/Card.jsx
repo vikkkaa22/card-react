f3aa30d213db220957aad383127dcfd765b30cb7
vikkkaa22 <kusya2203@gmail.com>
2026-04-21 20:07:56 +0500

    создание карточек

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react';
-
+import "./Card.css";
 
 export function Card({ front, back }) {
-    const [checkCard, setCheckCard] = React.useState(false)
+    const [checkCard, setCheckCard] = useState(false);
 
     const handleClick = () => {
-        setCheckCard(!checkCard)
-    }
+        setCheckCard(!checkCard);
+    };
 
-    const className = `card ${checkCard ? 'card-check' : ''}`
+    const className = `card ${checkCard ? 'card-check' : ''}`;
 
     return (
-        <div className={className} onClick={handleClick} data-back={back}> {front}
+        <div className={className} onClick={handleClick}  data-back={back}>
+            {front}
         </div>
-    )
-
+    );
 }
--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,13 +1,48 @@
 import { useState } from "react";
-import cards from "../data.json"
-
+import cards from "../data.json";
 import { Card } from "./Card";
+import "./Set.css"
+
 export function Set() {
+    const [step, setStep] = useState(0);
+
+    const nextCard = () => {
+        if (step < cards.length - 1) {
+            setStep(step + 1);
+        }
+    };
+
+    const prevCard = () => {
+        if (step > 0) {
+            setStep(step - 1);
+        }
+    };
+
     return (
         <div>
-            <h2>Название набора</h2>
-            <Card front={cards[step].front} back={cards[step].back}/>
-            <div className="subtitle">элементы управления</div>
+            <h2>Цвета на английском</h2>
+
+            <Card
+                key={cards[step].id}
+                front={cards[step].front}
+                back={cards[step].back}
+            />
+
+            {/* СЧЁТЧИК */}
+            <div className="subtitle">
+                {step + 1} / {cards.length}
+            </div>
+
+            {/* СТРЕЛКИ */}
+            <div className="controls">
+                <button onClick={prevCard} disabled={step === 0}>
+                    ←
+                </button>
+
+                <button onClick={nextCard} disabled={step === cards.length - 1}>
+                    →
+                </button>
+            </div>
         </div>
-    )
+    );
 }
